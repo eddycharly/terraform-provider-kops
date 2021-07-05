d1ff0adb1d8bced92e49392ef3595525ec0a564b
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2021-07-05 21:23:15 +0200

    feat: add klog example

---
+++ examples/klog/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    kops = {
+      source  = "github/eddycharly/kops"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "kops" {
+  state_store = "s3://cluster.example.com"
+
+  klog {
+    verbosity {
+      value = 2
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.kops_kube_config.kube_config.server
+    username               = data.kops_kube_config.kube_config.kube_user
+    password               = data.kops_kube_config.kube_config.kube_password
+    client_certificate     = data.kops_kube_config.kube_config.client_cert
+    client_key             = data.kops_kube_config.kube_config.client_key
+    cluster_ca_certificate = data.kops_kube_config.kube_config.ca_cert
+  }
+}
