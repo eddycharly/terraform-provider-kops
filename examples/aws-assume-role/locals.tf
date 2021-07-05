b42fbe2e396af5be5919c86907b7874c1a1e92b1
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2021-07-05 21:20:56 +0200

    feat: improve aws examples

---
+++ examples/aws-assume-role/locals.tf
@@ -0,0 +1,35 @@
+locals {
+  masterType  = "t3.medium"
+  nodeType    = "t3.medium"
+  clusterName = "cluster.example.com"
+  dnsZone     = "example.com"
+  vpcId       = "vpc-id"
+  privateSubnets = [
+    {
+      subnetId = "private-subnet-0"
+      zone     = "zone-0"
+    },
+    {
+      subnetId = "private-subnet-1"
+      zone     = "zone-1"
+    },
+    {
+      subnetId = "private-subnet-1"
+      zone     = "zone-2"
+    },
+  ]
+  utilitySubnets = [
+    {
+      subnetId = "utility-subnet-0"
+      zone     = "zone-0"
+    },
+    {
+      subnetId = "utility-subnet-1"
+      zone     = "zone-1"
+    },
+    {
+      subnetId = "utility-subnet-1"
+      zone     = "zone-2"
+    },
+  ]
+}
