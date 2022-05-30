34d3b4e56548fd562fa515c6ab6d2f35264ef282
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-05-30 09:39:12 +0200

    feat: add version package (#606)
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

---
+++ pkg/version/version.go
@@ -0,0 +1,10 @@
+package version
+
+// These fields are set during an official build
+// Global vars set from command-line arguments
+var (
+	BuildVersion = "--"
+	BuildCommit  = "--"
+	BuildDate    = "--"
+	BuiltBy      = "--"
+)
